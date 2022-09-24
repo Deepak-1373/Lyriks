1fb79c4d99f2b289571abaf541befe798de08a6a
Deepak Kamat <dkamat.001@gmail.com>
2022-09-24 12:54:13 +0530

    feat: add seekbar component

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Controls } from './Controls';
 import { Player } from './Player';
+import { Seekbar } from './Seekbar';
 import { Track } from './Track';
 import { VolumeBar } from './VolumeBar';
 
