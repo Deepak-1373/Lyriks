7fbf90ec73009c209b140c8d89a83370f8b997c0
Deepak Kamat <dkamat.001@gmail.com>
2022-09-24 12:08:42 +0530

    feat: add controls component and react-icons

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export const MusicPlayer = () => {
-  return <div>index</div>;
+  return (
+    <div className='relative sm:px-12 px-8 w-full flex items-center justify-between'>
+      <Track />
+      <div className='flex-1 flex flex-col items-center justify-center'>
+        <Controls />
+        <Seekbar />
+        <Player />
+      </div>
+      <VolumeBar />
+    </div>
+  );
 };
