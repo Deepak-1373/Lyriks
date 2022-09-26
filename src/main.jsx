77498b40d3c619a67857b25d7a556003662a9853
Deepak Kamat <dkamat.001@gmail.com>
2022-09-26 19:22:56 +0530

    chore: add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { PlayerContextProvider } from './context/playerContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PlayerContextProvider>
-      <App />
-    </PlayerContextProvider>
+    <Router>
+      <PlayerContextProvider>
+        <App />
+      </PlayerContextProvider>
+    </Router>
   </React.StrictMode>
 );
