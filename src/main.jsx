53b026d66c26a95199a587b9af7c7e8c4230930f
Deepak Kamat <dkamat.001@gmail.com>
2022-09-26 20:50:56 +0530

    feat: initialise router to main page

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <PlayerContextProvider>
+    <PlayerContextProvider>
+      <Router>
         <App />
-      </PlayerContextProvider>
-    </Router>
+      </Router>
+    </PlayerContextProvider>
   </React.StrictMode>
 );
