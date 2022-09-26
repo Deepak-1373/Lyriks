29c8e66d8985fbef5de8988048f5a4d5b7feb1d0
Deepak Kamat <dkamat.001@gmail.com>
2022-09-26 20:49:46 +0530

    feat(init): songcard component

---
+++ src/components/Songcard.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Songcard = () => {
+  return <div>Songcard</div>;
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { Sidebar } from './Sidebar';
 export { Searchbar } from './Searchbar';
+export { Songcard } from './Songcard';
