172cba2faad2bbb39ca01637a07f4583de4dc932
Deepak Kamat <dkamat.001@gmail.com>
2022-10-03 11:42:54 +0530

    refactor: add real api data to the discover component

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { genres } from '../assets/constant';
-import { Songcard } from '../components';
+import { Loader, Songcard } from '../components';
+import { getTopCharts } from '../features/shazamCoreApi';
+import { usePlayer } from '../context/playerContext';
 
 export const Discover = () => {
-  return (
-    <div className='flex flex-col'>
-      <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
-        <h2 className='font-bold text-3xl text-white text-left'>Discover</h2>
-        <select
-          value=''
-          className='bg-black text-gray-300 p-3 t-xm rounded-lg outline-none sm:mt-0 mt-5'
-        >
-          {genres.map(({ title, value }) => (
-            <option key={value} value={value}>
-              {title}
-            </option>
-          ))}
-        </select>
-      </div>
+  const { activeSong, isPlaying } = usePlayer();
+  const [chartData, setChartData] = useState({
+    isLoading: true,
+    data: [],
+  });
 
-      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
-          <Songcard key={song} song={song} index={i} />
-        ))}
-      </div>
-    </div>
+  useEffect(() => {
+    try {
+      getTopCharts.then((res) =>
+        setChartData((prev) => ({ ...prev, isLoading: false, data: res.data }))
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <>
+      {chartData.isLoading ? (
+        <Loader title='Loading Songs...' />
+      ) : (
+        <div className='flex flex-col'>
+          <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+            <h2 className='font-bold text-3xl text-white text-left'>
+              Discover
+            </h2>
+            <select
+              onChange={() => {}}
+              value=''
+              className='bg-black text-gray-300 p-3 t-xm rounded-lg outline-none sm:mt-0 mt-5'
+            >
+              {genres.map(({ title, value }) => (
+                <option key={value} value={value}>
+                  {title}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+            {chartData.data.map((song, i) => (
+              <Songcard
+                key={song.key}
+                song={song}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                index={i}
+                data={chartData.data}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
