0e1dd6a4ef2f6c0bb2fb72035a582b9911acc57e
Deepak Kamat <dkamat.001@gmail.com>
2022-10-03 11:27:06 +0530

    feat: add play pause component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Sidebar } from './Sidebar';
 export { Searchbar } from './Searchbar';
 export { Songcard } from './Songcard';
 export { Loader } from './Loader';
+export { PlayPause } from './PlayPause';
