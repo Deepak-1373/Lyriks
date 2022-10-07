aeaaa6f180a1828cf35da5ca65de43fe7fba4a99
Deepak Kamat <dkamat.001@gmail.com>
2022-10-07 20:04:11 +0530

    feat: display navlinks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiCloseLine } from 'react-icons/ri';
 import { logo } from '../assets';
 import { links } from '../assets/constant';
-import { useState } from 'react';
+
+const NavLinks = ({ handleClick }) => (
+  <div className='mt-10'>
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400'
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className='w-6 h-6 mr-2' />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
 
 export const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -10,6 +26,7 @@ export const Sidebar = () => {
     <>
       <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
         <img src={logo} alt={logo} className='w-full h-14 object-contain' />
+        <NavLinks />
       </div>
     </>
   );
