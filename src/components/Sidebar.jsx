848d36a99ef6e595068fbcfc7d9869416b7fb157
Deepak Kamat <dkamat.001@gmail.com>
2022-10-07 19:39:08 +0530

    feat: add sidebar logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+import { logo } from '../assets';
+import { links } from '../assets/constant';
+import { useState } from 'react';
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <>
+      <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
+        <img src={logo} alt={logo} className='w-full h-14 object-contain' />
+      </div>
+    </>
+  );
 };
