5a5dd6afc247341559af9251a362e309cc8d7ae9
Deepak Kamat <dkamat.001@gmail.com>
2022-10-11 14:41:27 +0530

    fix: classname in songCard Component

--- src/components/Songcard.jsx
+++ src/components/Songcard.jsx
@@ -16,12 +16,12 @@ export const Songcard = ({ song, isPlaying, activeSong, i, data }) => {
     dispatch(playPause(true));
   };
   return (
-    <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-50 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
+    <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
       <div className='relative w-full h-56 group'>
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
             activeSong?.title === song.title
-              ? 'flex bg-black bg-opacity-60'
+              ? 'flex bg-black bg-opacity-70'
               : 'hidden'
           }`}
         >
