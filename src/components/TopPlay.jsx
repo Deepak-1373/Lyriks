7f8dd2c1ede639238c9c29b521953c1181a00e5b
Deepak Kamat <dkamat.001@gmail.com>
2022-10-11 14:14:42 +0530

    feat: add swiper and swiper slide for the top artist preview in topPlay component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -20,7 +20,7 @@ const TopChartCard = ({ song, i }) => {
 export const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.musicPlayer);
-  const { data } = useGetTopChartsQuery;
+  const { data } = useGetTopChartsQuery();
   const divRef = useRef(null);
 
   useEffect(() => {
@@ -55,6 +55,41 @@ export const TopPlay = () => {
             <TopChartCard key={song.key} song={song} i={i} />
           ))}
         </div>
+      </div>
+
+      <div className='w-full flex flex-col mt-8 '>
+        <div className='flex flex-row justify-between items-center'>
+          <h2 className='text-white font-bold text-2xl'>Top Artists</h2>
+          <Link to='/top-artists'>
+            <p className='text-gray-300 text-base cursor-pointer'>See more</p>
+          </Link>
+        </div>
+
+        <Swiper
+          slidesPerView='auto'
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className='mt-4'
+        >
+          {topPlays?.map((song, i) => (
+            <SwiperSlide
+              key={song?.key}
+              style={{ width: '25%', height: 'auto' }}
+              className='shadow-lg rounded-full animate-slideright'
+            >
+              <Link to={`/artists/${song?.artists[0].adamid}`}>
+                <img
+                  src={song?.images.background}
+                  alt='name'
+                  className='rounded-full w-full object-cover'
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
