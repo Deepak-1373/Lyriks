8d693bbe91866967ced3bd102121cefcf104ec33
Deepak Kamat <dkamat.001@gmail.com>
2022-10-13 21:23:08 +0530

    feat: complete AroundYou page

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useGetSongsByCountryQuery } from '../redux/services/shazamCoreApi';
+import { Loader, Songcard } from '../components';
 import axios from 'axios';
 
 export const AroundYou = () => {
@@ -19,5 +20,23 @@ export const AroundYou = () => {
   }, [country]);
 
   if (isFetching && loading) return <Loader title='Loading songs around you' />;
-  return <div>AroundYou</div>;
+  return (
+    <div className='flex flex-col'>
+      <h2 className='font-bold text-2xl text-white text-left mt-4 mb-10'>
+        Around You <span className='font-black'>{country}</span>
+      </h2>
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {data?.map((song, i) => (
+          <Songcard
+            key={song.key}
+            song={song}
+            i={i}
+            data={data}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
