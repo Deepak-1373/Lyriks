036a229064d726751dde98c20c6b781f60f85ff8
Deepak Kamat <dkamat.001@gmail.com>
2022-10-13 20:14:55 +0530

    feat: finish RelatedSong component feature

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,3 +1,30 @@
-export const RelatedSongs = () => {
-  return <div>RelatedSongs</div>;
+import { SongBar } from './SongBar';
+
+export const RelatedSongs = ({
+  data,
+  isPlaying,
+  artistId,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => {
+  return (
+    <div className='flex flex-col'>
+      <h1 className='font-bold text-3xl text-white'>Related Songs:</h1>
+      <div className='mt-6 w-full flex flex-col'>
+        {data?.map((song, i) => (
+          <SongBar
+            key={`${song.key}-${artistId}`}
+            song={song}
+            i={i}
+            artistId={artistId}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
