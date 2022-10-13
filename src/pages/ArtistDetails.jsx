7d765b55b1863c48815b7fd8b2b625ad63880dcc
Deepak Kamat <dkamat.001@gmail.com>
2022-10-13 20:36:52 +0530

    feat: complete ArtistDetails page with layout and function for each particular artist

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,3 +1,35 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { DetailsHeader, RelatedSongs, Loader } from '../components';
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCoreApi';
+
 export const ArtistDetails = () => {
-  return <div>ArtistDetails</div>;
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.musicPlayer);
+  const { data: artistData, isFetching: isFetchingArtistDetails } =
+    useGetArtistDetailsQuery(artistId);
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  if (isFetchingArtistDetails) return <Loader title='Loading artist details' />;
+
+  return (
+    <div className='flex flex-col'>
+      <DetailsHeader artistId={artistId} artistData={artistData} />
+
+      <RelatedSongs
+        data={Object.values(artistData?.songs)}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
 };
