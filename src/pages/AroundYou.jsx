e2f12e7d97c2ade6117bb11e75ce34a5ad7a912e
Deepak Kamat <dkamat.001@gmail.com>
2022-10-13 21:15:41 +0530

    feat: add function for fetching country code around you

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCoreApi';
+import axios from 'axios';
+
 export const AroundYou = () => {
+  const geoIpifyKey = import.meta.env.VITE_GEOIPIY_API_KEY;
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.musicPlayer);
+  const { data, isFetching } = useGetSongsByCountryQuery(country);
+
+  useEffect(() => {
+    axios
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=${geoIpifyKey}`)
+      .then((res) => setCountry(res?.data?.location?.country))
+      .catch((err) => console.warn(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+
+  if (isFetching && loading) return <Loader title='Loading songs around you' />;
   return <div>AroundYou</div>;
 };
