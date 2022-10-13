95a5b20f65b13028e44e7315c12e48b6a31e2fae
Deepak Kamat <dkamat.001@gmail.com>
2022-10-13 19:17:21 +0530

    feat: add function to show lyrics in the songDetails page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,17 +1,36 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery } from '../redux/services/shazamCoreApi';
 
 export const SongDetails = () => {
   const { songid } = useParams();
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.musicPlayer);
+  const { data: songData, isFetching: isFetchingSongDetails } =
+    useGetSongDetailsQuery({ songid });
 
   return (
     <div className='flex flex-col'>
-      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <DetailsHeader artistId={artistId} songData={songData} />
       <div className='mb-10'>
         <h2 className='text-white text-3xl font-bold'>Lyrics:</h2>
+        <div className='mt-5'>
+          {songData?.sections[1].type === 'LYRICS' ? (
+            songData?.sections[1].text.map((line, i) => (
+              <p
+                key={`lyrics-${line}-${i}`}
+                className='text-gray-400 text-base my-1'
+              >
+                {line}
+              </p>
+            ))
+          ) : (
+            <p className='text-gray-400 text-base my-1'>
+              Sorry, no lyrics found
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
