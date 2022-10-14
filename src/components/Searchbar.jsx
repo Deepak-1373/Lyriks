7ec09fcbd407ceabf1c8b49834c4dea92a4ceada
Deepak Kamat <dkamat.001@gmail.com>
2022-10-14 12:38:56 +0530

    feat: add layout and styles for searchbar component

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FiSearch } from 'react-icons/fi';
 
 export const Searchbar = () => {
-  return <div>Searchbar</div>;
+  return (
+    <form
+      className='p-2 text-gray-400 focus-within:text-gray-600'
+      autoComplete='off'
+    >
+      <label htmlFor='search-field' className='sr-only'>
+        Search all songs
+      </label>
+      <div className='flex flex-row justify-start items-center'>
+        <FiSearch className='w-5 h-5 ml-4' />
+        <input
+          type='text'
+          name='search-field'
+          className='flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4'
+          autoComplete='off'
+          id='search-field'
+          placeholder='Search'
+          value=''
+          onChange={() => {}}
+        />
+      </div>
+    </form>
+  );
 };
