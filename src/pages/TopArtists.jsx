75720fe8e54974b37461281f1195b8fc2b7a4a5b
Deepak Kamat <dkamat.001@gmail.com>
2022-10-14 12:31:03 +0530

    feat: complete layout and functionality for TopArtists page

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,20 @@
+import { Loader, ArtistCard } from '../components';
+import { useGetTopChartsQuery } from '../redux/services/shazamCoreApi';
+
 export const TopArtists = () => {
-  return <div>TopArtists</div>;
+  const { data, isFetching } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title='Loading top charts' />;
+  return (
+    <div className='flex flex-col'>
+      <h2 className='font-bold text-2xl text-white text-left mt-4 mb-10'>
+        Top Artists
+      </h2>
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {data?.map((track) => (
+          <ArtistCard key={track.key} track={track} ArtistCard />
+        ))}
+      </div>
+    </div>
+  );
 };
