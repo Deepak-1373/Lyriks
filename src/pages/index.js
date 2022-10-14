f5720899eb444778e32c7ca5c46e34d93c80846f
Deepak Kamat <dkamat.001@gmail.com>
2022-10-14 12:19:53 +0530

    chore: init TopArtists page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { SongDetails } from './SongDetails';
 export { ArtistDetails } from './ArtistDetails';
 export { AroundYou } from './AroundYou';
 export { TopCharts } from './TopCharts';
+export { TopArtists } from './TopArtists';
