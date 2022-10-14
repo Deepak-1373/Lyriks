a994a93e5562044b72e7d3583d9c239d8f5fa623
Deepak Kamat <dkamat.001@gmail.com>
2022-10-14 13:07:49 +0530

    feat: complete layout and function for search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,33 @@
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Loader, Songcard } from '../components';
+import { useGetSongsBySearchQuery } from '../redux/services/shazamCoreApi';
+
 export const Search = () => {
-  return <div>Search</div>;
+  const { searchTerm } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.musicPlayer);
+  const { data, isFetching } = useGetSongsBySearchQuery(searchTerm);
+
+  const songs = data?.tracks?.hits?.map((song) => song.track);
+
+  if (isFetching) return <Loader title='Loading results' />;
+  return (
+    <div className='flex flex-col'>
+      <h2 className='font-bold text-2xl text-white text-left mt-4 mb-10'>
+        Showing results for <span className='font-black'>{searchTerm}</span>
+      </h2>
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {songs?.map((song, i) => (
+          <Songcard
+            key={song.key}
+            song={song}
+            i={i}
+            data={data}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
