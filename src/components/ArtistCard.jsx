d70a193215d149d43a6ad0e4a73e28362808eb0e
Deepak Kamat <dkamat.001@gmail.com>
2022-10-14 12:26:17 +0530

    feat: add layout and styles for ArtistCard component

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,3 +1,17 @@
-export const ArtistCard = () => {
-  return <div>ArtistCard</div>;
+import { useNavigate } from 'react-router-dom';
+
+export const ArtistCard = ({ track }) => {
+  const navigate = useNavigate();
+  return (
+    <div className='flex flex-col w-[200px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
+      <img
+        className='w-full h-56 rounded-lg'
+        src={track?.images?.coverart}
+        alt='artist'
+      />
+      <p className='mt-4 font-semibold text-lg text-white truncate'>
+        {track?.subtitle}
+      </p>
+    </div>
+  );
 };
