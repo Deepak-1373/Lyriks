2e015cb8ab9675a6dc74fe367c2743863e1d925d
Deepak Kamat <dkamat.001@gmail.com>
2022-10-14 12:30:38 +0530

    feat: complete functionality of navigation to any particular artist by having an onClick function

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -3,7 +3,10 @@ import { useNavigate } from 'react-router-dom';
 export const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
   return (
-    <div className='flex flex-col w-[200px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
+    <div
+      className='flex flex-col w-[200px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'
+      onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+    >
       <img
         className='w-full h-56 rounded-lg'
         src={track?.images?.coverart}
